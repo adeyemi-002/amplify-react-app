cec17dc9091dc3a4a27d7386f9a75c2e3d2ff444
adeyemi <ade.sobaki@gmail.com>
2022-06-29 16:13:22 +0200

    navbar link to cognito ui

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom";
-import "./navbar.css";
+import { Link } from 'react-router-dom'
+import './navbar.css'
+
+const handleclick = (e) => {
+  e.preventDefault()
+  console.log('redirect to AWS Cognito Ui')
+  window.location.href =
+    'https://mhp-co2-tracker.auth.eu-central-1.amazoncognito.com/login?client_id=3d63aajjll8vqcjh49f0uqn24s&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https%3A%2F%2Fmaster.d2ii4k9ar1ox2r.amplifyapp.com%2Fuserdashboard%2Fuser-dashboard.js%2F'
+}
 
 function NavBar() {
   return (
-    <div id="topnav">
-      <Link to="/signin/signin.js" className="pages">
+    <div id='topnav'>
+      <button onClick={handleclick} to='/signin/signin.js' className='pages'>
         Sign In
-      </Link>
+      </button>
     </div>
-  );
+  )
 }
 
-export default NavBar;
+export default NavBar
