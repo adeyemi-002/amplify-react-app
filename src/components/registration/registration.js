4dc78f6ab60442cd07fe8746196135ce50eb7dd6
adeyemi <ade.sobaki@gmail.com>
2022-08-04 10:45:55 +0200

    registration endpoint

--- src/components/registration/registration.js
+++ src/components/registration/registration.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import "./registration.css";
-import { Formik, Form, Field } from "formik";
+import { useState } from 'react'
+import './registration.css'
+import { Formik, Form, Field } from 'formik'
 
 function RegistrationForm() {
-  const [inputs, setInputs] = useState({});
+  const [inputs, setInputs] = useState({})
 
   // function handleSubmit(event) {
   //   event.preventDefault();
@@ -17,25 +17,41 @@ function RegistrationForm() {
   // }
 
   const initialValues = {
-    username: "",
-    email: "",
-    phone: "",
-  };
+    username: '',
+    email: '',
+    phone: '',
+  }
 
   return (
-    <div id="container">
+    <div id='container'>
       <Formik
         initialValues={initialValues}
         onSubmit={(values, { setSubmitting }) => {
-          console.log(values);
+          console.log(values)
+          axios
+            .post(
+              'https://gxktmecngi.execute-api.eu-central-1.amazonaws.com/dev/registration',
+              values,
+              {
+                headers: {
+                  'content-type': 'application/json',
+                },
+              }
+            )
+            .then(function (response) {
+              console.log(response)
+            })
+            .catch(function (error) {
+              console.log(error)
+            })
           setTimeout(() => {
             // alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 700);
+            setSubmitting(false)
+          }, 700)
         }}
       >
         {({ values, isSubmitting }) => (
-          <Form id="registration">
+          <Form id='registration'>
             {/* <div className="details">
             <label htmlFor="orgname" className="col-leftr">
               Organization Name:
@@ -50,16 +66,16 @@ function RegistrationForm() {
             <br></br>
           </div> */}
 
-            <div className="details">
-              <label htmlFor="firstname" className="col-leftr">
+            <div className='details'>
+              <label htmlFor='firstname' className='col-leftr'>
                 Username:
               </label>
               <Field
-                type="text"
-                name="username"
+                type='text'
+                name='username'
                 // value={inputs.firstname || ""}
                 // onChange={handleChange}
-                className="form-elect-input"
+                className='form-elect-input'
               />
               <br></br>
             </div>
@@ -78,45 +94,45 @@ function RegistrationForm() {
             <br></br>
           </div> */}
 
-            <div className="details">
-              <label htmlFor="email" className="col-leftr">
+            <div className='details'>
+              <label htmlFor='email' className='col-leftr'>
                 Email Address:
               </label>
               <Field
-                type="text"
-                name="email"
+                type='text'
+                name='email'
                 // onChange={handleChange}
-                className="form-elect-input"
+                className='form-elect-input'
               />
               <br></br>
             </div>
 
-            <div className="details">
-              <label htmlFor="phone" className="col-leftr">
+            <div className='details'>
+              <label htmlFor='phone' className='col-leftr'>
                 Phone Number:
               </label>
               <Field
-                type="text"
-                name="phone"
+                type='text'
+                name='phone'
                 // onChange={handleChange}
-                className="form-elect-input"
+                className='form-elect-input'
               />
               <br></br>
             </div>
 
-            <div id="btn">
+            <div id='btn'>
               {/* <button type="reset" id="cancel">
                 Cancel
               </button> */}
-              <button type="submit" id="register" disabled={isSubmitting}>
-                {isSubmitting ? "Loading..." : "Register"}
+              <button type='submit' id='register' disabled={isSubmitting}>
+                {isSubmitting ? 'Loading...' : 'Register'}
               </button>
             </div>
           </Form>
         )}
       </Formik>
     </div>
-  );
+  )
 }
 
-export default RegistrationForm;
+export default RegistrationForm
