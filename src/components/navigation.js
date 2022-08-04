736cb6c6eae62e74b30b9e9ff8d3379e49edd5a5
adeyemi <ade.sobaki@gmail.com>
2022-08-04 09:39:59 +0200

    04.08

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -15,99 +15,111 @@ import NavBar from './navbar'
 import { Formik, Form, Field } from 'formik'
 
 function Navigation() {
-  return (
-    <div>
-      <Router basename='/' id='container'>
-        <NavBar />
-        <div id='links'>
-          <div>
-            <Link to='/' className='pages'>
-              Home
-            </Link>
-            <Link to='/registration/registration.js' className='pages'>
-              Registration
-            </Link>
-            <Link to='/admin/admin-login.js' className='pages'>
-              Admin
-            </Link>
-            <Link to='/adminconsole/admin-console.js' className='pages'>
-              Adminconsole
-            </Link>
-            <Link to='/userdashboard/user-dashboard.js' className='pages'>
-              Userdashboard
-            </Link>
-            <Link
-              to='/userdashboardairplane/user-dasboard-airplane.js'
-              className='pages'
-            >
-              UserdashPlane
-            </Link>
-            <Link
-              to='/userdashboardbus/user-dashboard-bus.js'
-              className='pages'
-            >
-              UserdashBus
-            </Link>
-            <Link to='/userdashboardcar/user-dasboard-car.js' className='pages'>
-              UserdashCar
-            </Link>
-            <Link
-              to='/userdashboardhouse/user-dasboard-house.js'
-              className='pages'
-            >
-              UserdashHouse
-            </Link>
-            <Link
-              to='/userdashboardtrain/user-dasboard-train.js'
-              className='pages'
-            >
-              UserdashTrain
-            </Link>
-          </div>
-          <div>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/home/home.js' element={<Home />} />
-              <Route
-                path='/registration/registration.js'
-                element={<Registration />}
-              />
-              <Route path='/signin/signin.js' element={<SignIn />} />
-              <Route path='/admin/admin-login.js' element={<AdminLogin />} />
-              <Route
-                path='/adminconsole/admin-console.js'
-                element={<AdminConsole />}
-              />
-              <Route
-                path='/userdashboard/user-dashboard.js'
-                element={<UserDashboard />}
-              />
-              <Route
-                path='/userdashboardairplane/user-dasboard-airplane.js'
-                element={<AirplaneDashboard />}
-              />
-              <Route
-                path='/userdashboardbus/user-dashboard-bus.js'
-                element={<BusDashboard />}
-              />
-              <Route
-                path='/userdashboardcar/user-dasboard-car.js'
-                element={<CarDashboard />}
-              />
-              <Route
-                path='/userdashboardhouse/user-dasboard-house.js'
-                element={<HouseDashboard />}
-              />
-              <Route
-                path='/userdashboardtrain/user-dasboard-train.js'
-                element={<TrainDashboard />}
-              />
-            </Routes>
-          </div>
-        </div>
-      </Router>
-    </div>
-  )
+	const loggedIn = localStorage.getItem('loggedIn')
+
+	return (
+		<div>
+			<Router basename='/' id='container'>
+				<NavBar />
+				<div id='links'>
+					{loggedIn ? (
+						<div>
+							<Link to='/home/home.js' className='pages'>
+								Home
+							</Link>
+							<Link to='/registration/registration.js' className='pages'>
+								Registration
+							</Link>
+							<Link to='/admin/admin-login.js' className='pages'>
+								Admin
+							</Link>
+							{/*
+					<Link to='/adminconsole/admin-console.js' className='pages'>
+						Adminconsole
+					</Link>
+					*/}
+							<Link to='/userdashboard/user-dashboard.js' className='pages'>
+								Userdashboard
+							</Link>
+							<Link
+								to='/userdashboardairplane/user-dasboard-airplane.js'
+								className='pages'
+							>
+								UserdashPlane
+							</Link>
+							<Link
+								to='/userdashboardbus/user-dashboard-bus.js'
+								className='pages'
+							>
+								UserdashBus
+							</Link>
+							<Link
+								to='/userdashboardcar/user-dasboard-car.js'
+								className='pages'
+							>
+								UserdashCar
+							</Link>
+							<Link
+								to='/userdashboardhouse/user-dasboard-house.js'
+								className='pages'
+							>
+								UserdashHouse
+							</Link>
+							<Link
+								to='/userdashboardtrain/user-dasboard-train.js'
+								className='pages'
+							>
+								UserdashTrain
+							</Link>
+						</div>
+					) : (
+						<div></div>
+					)}
+
+					<div>
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/home/home.js' element={<Home />} />
+							<Route
+								path='/registration/registration.js'
+								element={<Registration />}
+							/>
+							<Route path='/signin/signin.js' element={<SignIn />} />
+							<Route path='/admin/admin-login.js' element={<AdminLogin />} />
+							<Route
+								path='/adminconsole/admin-console.js'
+								element={<AdminConsole />}
+							/>
+							<Route
+								path='/userdashboard/user-dashboard.js'
+								element={<UserDashboard />}
+							/>
+							<Route
+								path='/userdashboardairplane/user-dasboard-airplane.js'
+								element={<AirplaneDashboard />}
+							/>
+							<Route
+								path='/userdashboardbus/user-dashboard-bus.js'
+								element={<BusDashboard />}
+							/>
+							<Route
+								path='/userdashboardcar/user-dasboard-car.js'
+								element={<CarDashboard />}
+							/>
+							<Route
+								path='/userdashboardhouse/user-dasboard-house.js'
+								element={<HouseDashboard />}
+							/>
+							<Route
+								path='/userdashboardtrain/user-dasboard-train.js'
+								element={<TrainDashboard />}
+							/>
+						</Routes>
+					</div>
+				</div>
+			</Router>
+		</div>
+	)
 }
 
 export default Navigation
--- src/components/registration/registration.js
+++ src/components/registration/registration.js
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import './registration.css'
-import { Formik, Form, Field } from 'formik'
-import axios from 'axios'
+import { useState } from "react";
+import "./registration.css";
+import { Formik, Form, Field } from "formik";
 
 function RegistrationForm() {
-  const [inputs, setInputs] = useState({})
+  const [inputs, setInputs] = useState({});
 
   // function handleSubmit(event) {
   //   event.preventDefault();
@@ -18,41 +17,25 @@ function RegistrationForm() {
   // }
 
   const initialValues = {
-    username: '',
-    email: '',
-    phone: '',
-  }
+    username: "",
+    email: "",
+    phone: "",
+  };
 
   return (
-    <div id='container'>
+    <div id="container">
       <Formik
         initialValues={initialValues}
         onSubmit={(values, { setSubmitting }) => {
-          console.log(values)
-          axios
-            .post(
-              'https://gxktmecngi.execute-api.eu-central-1.amazonaws.com/dev/registration',
-              values,
-              {
-                headers: {
-                  'content-type': 'application/json',
-                },
-              }
-            )
-            .then(function (response) {
-              console.log(response)
-            })
-            .catch(function (error) {
-              console.log(error)
-            })
+          console.log(values);
           setTimeout(() => {
             // alert(JSON.stringify(values, null, 2));
-            setSubmitting(false)
-          }, 700)
+            setSubmitting(false);
+          }, 700);
         }}
       >
         {({ values, isSubmitting }) => (
-          <Form id='registration'>
+          <Form id="registration">
             {/* <div className="details">
             <label htmlFor="orgname" className="col-leftr">
               Organization Name:
@@ -67,16 +50,16 @@ function RegistrationForm() {
             <br></br>
           </div> */}
 
-            <div className='details'>
-              <label htmlFor='firstname' className='col-leftr'>
+            <div className="details">
+              <label htmlFor="firstname" className="col-leftr">
                 Username:
               </label>
               <Field
-                type='text'
-                name='username'
+                type="text"
+                name="username"
                 // value={inputs.firstname || ""}
                 // onChange={handleChange}
-                className='form-elect-input'
+                className="form-elect-input"
               />
               <br></br>
             </div>
@@ -95,45 +78,45 @@ function RegistrationForm() {
             <br></br>
           </div> */}
 
-            <div className='details'>
-              <label htmlFor='email' className='col-leftr'>
+            <div className="details">
+              <label htmlFor="email" className="col-leftr">
                 Email Address:
               </label>
               <Field
-                type='text'
-                name='email'
+                type="text"
+                name="email"
                 // onChange={handleChange}
-                className='form-elect-input'
+                className="form-elect-input"
               />
               <br></br>
             </div>
 
-            <div className='details'>
-              <label htmlFor='phone' className='col-leftr'>
+            <div className="details">
+              <label htmlFor="phone" className="col-leftr">
                 Phone Number:
               </label>
               <Field
-                type='text'
-                name='phone'
+                type="text"
+                name="phone"
                 // onChange={handleChange}
-                className='form-elect-input'
+                className="form-elect-input"
               />
               <br></br>
             </div>
 
-            <div id='btn'>
+            <div id="btn">
               {/* <button type="reset" id="cancel">
                 Cancel
               </button> */}
-              <button type='submit' id='register' disabled={isSubmitting}>
-                {isSubmitting ? 'Loading...' : 'Register'}
+              <button type="submit" id="register" disabled={isSubmitting}>
+                {isSubmitting ? "Loading..." : "Register"}
               </button>
             </div>
           </Form>
         )}
       </Formik>
     </div>
-  )
+  );
 }
 
-export default RegistrationForm
+export default RegistrationForm;
